9d0a31e6b02fd0a511614c46d3cbffa6af385895
Diana <dsiguenza@krugerschool.edu.ec>
2024-05-17 20:26:01 -0500

    cambios finales viernes

--- js/login.js
+++ js/login.js
@@ -1,38 +1,52 @@
-/*
-//////////////////////////////////////////////
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
 
-RentEase 2024
+    <div id="profile" style="display: none;">
+        <img src="./profile.png" alt="login">
+    <br/>
+    <span id="mensaje"></span>
+    </div>
 
-Luis F. Suarez
-Diana Siguenza
+    <div id="autenticación" style="display: block;">
+        <input type="text" id="usuario" placeholder="Ingrese su usuario" />
+        <br/>
+        <input type="password" id="contraseña" placeholder="Ingrese su contraseña" />
+        <br/> <br/>
+        <button onclick="procesarClic();">Iniciar sesión"</button>
+    </div>
 
-Email String
-Password String
-First_Name String
-Last_Name String
-Birth_Date_Date
-
-//////////////////////////////////////////////
-*/
-//prototipo
-class User {
-	constructor(email,password,First_Name,Last_Name,Birth_Date_Date) {
-		this.email = email;
-		this.password = password;
-		this.First_Name = First_Name;
-		this.last_Name = Last_Name;
-		this.Birth_Date_Date = Birth_Date_Date;
-	}
-	unionData(){
-
-		var User = JSON.parse(localStorage.getItem('User'));
-	}
-	saveUser(){
-		this.email.push
-	}
-}
-
-const user = new User();
-User.
-
-
+    <script>
+        var user = {
+                "login":"",
+                "contraseña":"",
+                profile:{
+                    id: 0,
+                    nameProfile:'',
+                }
+        }
+        var procesarClic = function(){
+            var user1 = document.getElementById('usuario').value;
+            var contraseña1 = document.getElementById('contraseña').value;
+        if(contraseña1 == "1234"){
+            user.profile.nameProfile="admin";
+            user.profile.id=5;
+            user.login = user1;
+            user.contraseña = contraseña1;
+            document.getElementById('profile').style = "display:block";
+            document.getElementById('autenticación').style = "display:none";
+            document.getElementById('mensaje').innerHTML = "Bienvenid@" + user.login + "Usted es el usuario con el rol:" +user.profile.nameProfile;
+        }
+        else{
+           alert('La clave ingresada es inválida. Inténtalo nuevamente.');
+        }
+    }
+      
+    </script>
+</body>
+</html>
